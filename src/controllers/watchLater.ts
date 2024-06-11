d3636c9d41f7aa1767756c255d81c2bf730023af
Mateo_choy <119556627+vizcacha22@users.noreply.github.com>
2024-06-11 00:07:58 -0500

    Ver más tarde con colas

---
+++ src/controllers/watchLater.ts
@@ -0,0 +1,58 @@
+class Pila<T>{
+    private items: T[];
+
+    constructor(){
+        this.items = [];
+    }
+
+    añadir(item: T): void{
+        this.items.push(item)
+    }
+    quitar(): T | undefined {
+        return this.items.shift();
+    }
+
+    ver(): T | undefined {
+        return this.items[0];
+    }
+
+    vacio(): boolean {
+        return this.items.length === 0;
+    }
+
+    cantidad(): number {
+        return this.items.length;
+    }
+
+    limpiar(): void {
+        this.items = [];
+    }
+}
+
+class WatchLater<T> {
+    private stack: Pila<T>;
+
+    constructor() {
+        this.stack = new Pila<T>();
+    }
+
+    añadirAVerMasTarde(item: T): void {
+        this.stack.añadir(item);
+    }
+
+    eliminarPelicula(): T | undefined {
+        return this.stack.quitar();
+    }
+
+    verUltimo(): T | undefined {
+        return this.stack.ver();
+    }
+
+    estaVacio(): boolean {
+        return this.stack.vacio();
+    }
+
+    obtenerNumeroElementos(): number {
+        return this.stack.cantidad();
+    }
+}
