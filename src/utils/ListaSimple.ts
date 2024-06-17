f54e5ff25a858e0b92470c5e8abfdeed14e7562f
Yayir <yayir.mattos@unmsm.edu.pe>
2024-06-17 17:20:33 -0500

    MergeSort(Peliculas)

---
+++ src/utils/ListaSimple.ts
@@ -0,0 +1,49 @@
+import { Movie } from "../schemas/movie";
+
+export class ListaSimple{
+    peliculas: Movie[] = [];
+
+    ordenarPeliculas(): void{
+        this.peliculas = this.mergeSort(this.peliculas);
+    }
+
+    private mergeSort(array: Movie[]): Movie[]{
+        if(array.length <= 1){
+            return array;
+        }
+
+        const middle = Math.floor(array.length / 2);
+        const left = array.slice(0, middle);
+        const right = array.slice(middle);
+
+        return this.merge(this.mergeSort(left), this.mergeSort(right));
+    }
+
+    private merge(left: Movie[], right: Movie[]): Movie[]{
+        let arrayOrdenado: Movie[] = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+
+        while(leftIndex < left.length && rightIndex < right.length){
+            if(left[leftIndex].title.toLowerCase() < right[rightIndex].title.toLowerCase()){
+                arrayOrdenado.push(left[leftIndex]);
+                leftIndex++;
+            }else{
+                arrayOrdenado.push(right[rightIndex]);
+                rightIndex++;
+            }
+        }
+
+        while(leftIndex < left.length){
+            arrayOrdenado.push(left[leftIndex]);
+            leftIndex++;
+        }
+
+        while(rightIndex < right.length){
+            arrayOrdenado.push(right[rightIndex]);
+            rightIndex++;
+        }
+
+        return arrayOrdenado;
+    }
+}
