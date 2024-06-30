5da9ad3f9de1ab78a116092112f41c3f0a62cbb1
DvKirbo <cuetosalazarsebas@gmail.com>
2024-06-30 01:14:44 -0500

    search home getMovieById

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -2,8 +2,9 @@ import {PrismaClient} from '@prisma/client';
 const prisma  = new PrismaClient();
 
 export async function allmovies(){
-    return await prisma.movie.findMany({
+    const movies = await prisma.movie.findMany({
         select: {
+            id: true,
             title: true,
             original_title: true,
             overview: true,            
@@ -13,5 +14,28 @@ export async function allmovies(){
             release_date: true,
             genres: true,
         },
+    });
+    return movies.map(movie => ({
+        ...movie,
+        id: movie.id.toString()
+    }))
+}
+
+export async function findMoviebyId(name: string){
+    const idBigInt = BigInt(name);
+    return await prisma.movie.findFirst({
+        where:{
+            id: idBigInt
+        },
+        select:{
+            title: true,
+            original_title: true,
+            overview: true,
+            Poster: true,
+            Backdrop: true,
+            popularity: true,
+            release_date: true,
+            genres: true,
+        }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,32 @@ import morgan from 'morgan'
 import authRouter from './routes/auth'
 import movies from './routes/movies'
 import cors from 'cors'
+import { ListaSimple } from './utils/ListaSimple'
+import { allmovies } from './controllers/movies'
+import { BinarySearchTree } from './utils/binaryTree'
+
+export const listaHome = new ListaSimple();
+export const binarytree = new BinarySearchTree(); 
+
+const initMovies = (listaHome : ListaSimple) =>{
+    allmovies().then(movies  => {
+        listaHome.peliculas = movies;
+        listaHome.ordenarPeliculas();
+        listaHome.peliculas.forEach( element =>{
+            binarytree.insert(element);
+            console.log(element.title);
+        })
+        console.log('Peliculas cargadas correctamente')
+    }).catch(error =>{
+        console.log("Error al cargar las peliculas");
+        console.log(error);
+    })
+}
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());//midleware para parsear todo a json
 app.use(morgan('dev'));
@@ -17,5 +40,6 @@ app.use('/movies', movies);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
+    initMovies(listaHome);
 })
 
